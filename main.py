f1774823f3173bea68938884749ed97664b154af
alanesige <104570972+alanesige@users.noreply.github.com>
2022-06-15 09:15:54 +0300

    Update main.py
    Works on Mkvs, Mp4s and Mp3s.

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import font
 from types import LambdaType
-import PyPDF2
-from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 import os
 import argparse
-import  tkinter.messagebox
 from fileinput import filename
-from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
-from PyPDF2.pdf import ContentStream
-from PyPDF4.generic import TextStringObject, NameObject
-from PyPDF4.utils import b_
 from pip import main
 
 import os
@@ -52,13 +45,82 @@ def awesome():
      for foldername, dirs, filenames in os.walk(txt):
             for filename in filenames:
 
-                Video_File = os.path.join(txt, filename)
-                print(Video_File)
-                new_filename = re.sub("FHR|1080p|y2mate\.com|audio|youtube|official|music|video|\[|\]|\,|\/|\\|\_|lyric|SMS|TO 811 FOR SKIZA|([0-9]{5,20})|directed by cole bennett|(\(\d.*\))|(\()|(\))|^(\-)|(\-)$|(\_)$|^([ ]{0,})|([ ]{0,})$|((\_).([ ]{0,}))$", "", filename, flags=re.IGNORECASE)
-                print(new_filename)
-                os.rename(os.path.join(txt,filename), os.path.join(txt,new_filename))
+                if filename.endswith('.mkv'):
+                  # continue
+
+                  Video_File = os.path.join(txt, filename)
                 
-             
+
+               
+                  new_filenames = re.sub("\.mkv|FHR|1080p|for skiza tune|FOR SKIZA TO 811|dial|send skiza|skiza|811|ixtendz|vxtendz|For Skiza  skiza  to 811|\'|\"|ogopa video|WSHH Exclusive|skiza to 811|hd|Tubidy.io|360p|480p||SabWap.CoM|Directed by WeL!iveTV|Prod. By JDONTHATRACK|Prod. by JDOnThaTrack|@prodbyyaygo|Fun Video|nrXtendz|Xtendz|Coke Studio Africa|(Intro Outro)|SNMiX|Dir.Ivan Odie|- _2|mpeg2|Ashawo|lucie|clean|via torchbrowser.com|Afrobeats|prod. by Team Salut - #FlavourzEP|Produced by Team Salut|GRM Daily|official mash up video|mpeg|Vibes Video|extended|beat|instrumental|ug remix|Baba nla|nr|xtends|YTMAs|Prod By- Tay Keith|y2mate\.com|audio|visualizer|Acoustic|QUARANTINE LOVE|Lyric|demo|Soundtrack|SpiderMan- Into The SpiderVerse Soundtrack|Produced by. StarboyUniverse|Prod. By Tony Fadd|Danielle Bregoli is|Prod. by Ulopa & Kus Ma|BEAT LINK|Mash Up|Ogede|emPawa Africa &|#emPawa100 Artist|Badder Than Bad|RH Exclusive|Gbadun You|Danielle Bregoli|Extended Version|2016|2017|Shot By- @Yoo Ali|Dir. Shooter-7-Seven|Dir Gerard Victor|Late Night with Seth Meyers|720p|London On Da Track|Prod. by JDOnThaTrack|Prod. by DJ Cause|I Know I'm Right Pt. 2|Directed by WeL!iveTV|- WSHH Exclusive -|from CREED- Original Motion Picture Soundtrack|The Album - Diamonds|720P HD|Spider-Man- Into the Spider-Verse|Freestyle|Quality Control|via torchbrowser.com|Prod. By Young Mercy DL|WSHH_Exclusive|hd720|By PDUB The Producer|Dir By JDFilms|Prod. by London on Tha Track|Into The SpiderVerse Soundtrack|THE SCOTTS FORTNITE ASTRONOMICAL EVENT|DN4|Oficial|Produced by. StarboyUniverse|WSHH Exclusive|youtube|Copy|official|music|Dir. by @_ColeBennett|video|\[|\]|\,|\/|\\|\_|lyric|remix|explicit|4k|SMS|wshh exclusve|Presented by @lakafilms|TO 811 FOR SKIZA|([0-9]{5,20})|directed by cole bennett", "", filename, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\(\d.*\))|(\()|(\))|^(\-)|(\-)$|(\_)$|^([ ]{0,})|([ ]{0,})$|((\_).([ ]{0,}))$|(\s\s*)$","",new_filenames, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\-)$","",new_filename, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("(\_)"," ",new_filename, flags=re.IGNORECASE)
+                  new_filenamerz = re.sub("- -","-",new_filenamer, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("\--","\-",new_filenamerz, flags=re.IGNORECASE)
+                  new_filenamers = re.sub("(\s\s*)$","",new_filenamer, flags=re.IGNORECASE)
+                
+                  testing = os.path.join(txt,new_filenamers+'.mkv')
+                  if os.path.exists(testing):
+                      swag = os.path.join(txt,new_filenamers+' v2 '+'.mkv')
+                      drake = os.rename(os.path.join(txt,filename), testing)
+                     #print('file already exist')
+                  else:
+                    drake = os.rename(os.path.join(txt,filename), testing)
+                    print('mkv')
+                
+                elif filename.endswith('.mp4'):
+                  # continue
+
+                  Video_File = os.path.join(txt, filename)
+                
+
+               
+                  new_filenames = re.sub("\.mp4|FHR|1080p|for skiza tune|FOR SKIZA TO 811|dial|send skiza|skiza|811|ixtendz|vxtendz|For Skiza  skiza  to 811|\'|\"|ogopa video|WSHH Exclusive|skiza to 811|hd|Tubidy.io|360p|480p||SabWap.CoM|Directed by WeL!iveTV|Prod. By JDONTHATRACK|Prod. by JDOnThaTrack|@prodbyyaygo|Fun Video|nrXtendz|Xtendz|Coke Studio Africa|(Intro Outro)|SNMiX|Dir.Ivan Odie|- _2|mpeg2|Ashawo|lucie|clean|via torchbrowser.com|Afrobeats|prod. by Team Salut - #FlavourzEP|Produced by Team Salut|GRM Daily|official mash up video|mpeg|Vibes Video|extended|beat|instrumental|ug remix|Baba nla|nr|xtends|YTMAs|Prod By- Tay Keith|y2mate\.com|audio|visualizer|Acoustic|QUARANTINE LOVE|Lyric|demo|Soundtrack|SpiderMan- Into The SpiderVerse Soundtrack|Produced by. StarboyUniverse|Prod. By Tony Fadd|Danielle Bregoli is|Prod. by Ulopa & Kus Ma|BEAT LINK|Mash Up|Ogede|emPawa Africa &|#emPawa100 Artist|Badder Than Bad|RH Exclusive|Gbadun You|Danielle Bregoli|Extended Version|2016|2017|Shot By- @Yoo Ali|Dir. Shooter-7-Seven|Dir Gerard Victor|Late Night with Seth Meyers|720p|London On Da Track|Prod. by JDOnThaTrack|Prod. by DJ Cause|I Know I'm Right Pt. 2|Directed by WeL!iveTV|- WSHH Exclusive -|from CREED- Original Motion Picture Soundtrack|The Album - Diamonds|720P HD|Spider-Man- Into the Spider-Verse|Freestyle|Quality Control|via torchbrowser.com|Prod. By Young Mercy DL|WSHH_Exclusive|hd720|By PDUB The Producer|Dir By JDFilms|Prod. by London on Tha Track|Into The SpiderVerse Soundtrack|THE SCOTTS FORTNITE ASTRONOMICAL EVENT|DN4|Oficial|Produced by. StarboyUniverse|WSHH Exclusive|youtube|Copy|official|music|Dir. by @_ColeBennett|video|\[|\]|\,|\/|\\|\_|lyric|remix|explicit|4k|SMS|wshh exclusve|Presented by @lakafilms|TO 811 FOR SKIZA|([0-9]{5,20})|directed by cole bennett", "", filename, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\(\d.*\))|(\()|(\))|^(\-)|(\-)$|(\_)$|^([ ]{0,})|([ ]{0,})$|((\_).([ ]{0,}))$|(\s\s*)$","",new_filenames, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\-)$","",new_filename, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("(\_)"," ",new_filename, flags=re.IGNORECASE)
+                  new_filenamerz = re.sub("- -","-",new_filenamer, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("\--","\-",new_filenamerz, flags=re.IGNORECASE)
+                  new_filenamers = re.sub("(\s\s*)$","",new_filenamer, flags=re.IGNORECASE)
+                
+                  testing = os.path.join(txt,new_filenamers+'.mp4')
+                  if os.path.exists(testing):
+                      swag = os.path.join(txt,new_filenamers+' v2 '+'.mp4')
+                      drake = os.rename(os.path.join(txt,filename), testing)
+                     #print('file already exist')
+                  else:
+                    drake = os.rename(os.path.join(txt,filename), testing)
+                    print('mp4')
+
+
+
+                elif filename.endswith('.mp3'):
+                  # continue
+
+                  Video_File = os.path.join(txt, filename)
+                
+
+               
+                  new_filenames = re.sub("\.mp3|FHR|1080p|for skiza tune|FOR SKIZA TO 811|dial|send skiza|skiza|811|ixtendz|vxtendz|For Skiza  skiza  to 811|\'|\"|ogopa video|WSHH Exclusive|skiza to 811|hd|Tubidy.io|360p|480p||SabWap.CoM|Directed by WeL!iveTV|Prod. By JDONTHATRACK|Prod. by JDOnThaTrack|@prodbyyaygo|Fun Video|nrXtendz|Xtendz|Coke Studio Africa|(Intro Outro)|SNMiX|Dir.Ivan Odie|- _2|mpeg2|Ashawo|lucie|clean|via torchbrowser.com|Afrobeats|prod. by Team Salut - #FlavourzEP|Produced by Team Salut|GRM Daily|official mash up video|mpeg|Vibes Video|extended|beat|instrumental|ug remix|Baba nla|nr|xtends|YTMAs|Prod By- Tay Keith|y2mate\.com|audio|visualizer|Acoustic|QUARANTINE LOVE|Lyric|demo|Soundtrack|SpiderMan- Into The SpiderVerse Soundtrack|Produced by. StarboyUniverse|Prod. By Tony Fadd|Danielle Bregoli is|Prod. by Ulopa & Kus Ma|BEAT LINK|Mash Up|Ogede|emPawa Africa &|#emPawa100 Artist|Badder Than Bad|RH Exclusive|Gbadun You|Danielle Bregoli|Extended Version|2016|2017|Shot By- @Yoo Ali|Dir. Shooter-7-Seven|Dir Gerard Victor|Late Night with Seth Meyers|720p|London On Da Track|Prod. by JDOnThaTrack|Prod. by DJ Cause|I Know I'm Right Pt. 2|Directed by WeL!iveTV|- WSHH Exclusive -|from CREED- Original Motion Picture Soundtrack|The Album - Diamonds|720P HD|Spider-Man- Into the Spider-Verse|Freestyle|Quality Control|via torchbrowser.com|Prod. By Young Mercy DL|WSHH_Exclusive|hd720|By PDUB The Producer|Dir By JDFilms|Prod. by London on Tha Track|Into The SpiderVerse Soundtrack|THE SCOTTS FORTNITE ASTRONOMICAL EVENT|DN4|Oficial|Produced by. StarboyUniverse|WSHH Exclusive|youtube|Copy|official|music|Dir. by @_ColeBennett|video|\[|\]|\,|\/|\\|\_|lyric|remix|explicit|4k|SMS|wshh exclusve|Presented by @lakafilms|TO 811 FOR SKIZA|([0-9]{5,20})|directed by cole bennett", "", filename, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\(\d.*\))|(\()|(\))|^(\-)|(\-)$|(\_)$|^([ ]{0,})|([ ]{0,})$|((\_).([ ]{0,}))$|(\s\s*)$","",new_filenames, flags=re.IGNORECASE)
+                  new_filename = re.sub("(\-)$","",new_filename, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("(\_)"," ",new_filename, flags=re.IGNORECASE)
+                  new_filenamerz = re.sub("- -","-",new_filenamer, flags=re.IGNORECASE)
+                  new_filenamer = re.sub("\--","\-",new_filenamerz, flags=re.IGNORECASE)
+                  new_filenamers = re.sub("(\s\s*)$","",new_filenamer, flags=re.IGNORECASE)
+                
+                  testing = os.path.join(txt,new_filenamers+'.mp3')
+                  if os.path.exists(testing):
+                      swag = os.path.join(txt,new_filenamers+' v2 '+'.mp3')
+                      drake = os.rename(os.path.join(txt,filename), testing)
+                     #print('file already exist')
+                  else:
+                    drake = os.rename(os.path.join(txt,filename), testing)
+                    print('mp3')
+
+                else:
+                    continue
      #esige.messagebox.showinfo("Hurray!","Renaming Completed.")
 
     
